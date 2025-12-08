edded076e0bc1f4ab6b20eb75e7fa0d98368e3c2
kriswangsaputra <kriswangsaputra04@gmail.com>
2025-12-08 20:23:25 +0700

    kalkulator keuangan

--- js/header-footer.js
+++ js/header-footer.js
@@ -1,20 +1,38 @@
 // ini part header
 export function header() {
     return `
-    <nav class="navbar">
-        <div class="container-fluid">
-          <a class="navbar-brand">
-            <img
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="index.html">
+          <img
               src="img/logo01.webp"
               alt="Logo fintrack"
               width="200"
               height="auto"
               class="d-inline-block align-text-top"
             />
-          </a>
-          <button class="btn btn-outline-primary" type="submit">Masuk</button>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="index.html">Beranda</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="modul.html">Materi Mendalam</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="kalkulator-keuangan.html">Kalkulator Keuangan</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Berita</a>
+            </li>
+          </ul>
         </div>
-      </nav>
+      </div>
+    </nav>
     `;
 };
 
